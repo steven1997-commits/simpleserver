73d3a1c1a93143cbcc7b21ad2ac0003a2f85bc8f
steven1997-commits <54122929+steven1997-commits@users.noreply.github.com>
2019-08-19 17:13:31 -0300

    Update client.sh

--- client.sh
+++ client.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 #Run script for client distributed as part of 
-#Assignment 1
-#Computer Networks (CS 456/656)
 #Number of parameters: 4
 #Parameter:
 #    $1: <server_address>
@@ -10,16 +8,5 @@
 #    $3: <req_code>
 #    $4: message
 
-#Uncomment exactly one of the following commands depending on your implementation
-
-#For C/C++ implementation
-#./client $1 $2 $3 "$4"
-
-#For Java implementation
-#java client $1 $2 $3 "$4"
-
 #For Python implementation
 python client.py $1 $2 $3 "$4"
-
-#For Ruby implementation
-#ruby client.rb $1 $2 $3 "$4"
